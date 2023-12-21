fffd4749713452ec26c3055a07a7d68dbb5f87b2
goisot <hugo.goisot@lyceeastier.com>
2023-12-21 09:23:32 +0100

    version avec affichage fini mais il faut retravailler les methodes

--- src/main/java/fr/btsciel/GestionVoiture.java
+++ src/main/java/fr/btsciel/GestionVoiture.java
@@ -1,9 +1,11 @@
 package fr.btsciel;
 
+import clavier.In;
+
 import java.sql.*;
 import java.util.ArrayList;
 
-public class GestionVoiture {
+public class GestionVoiture{
     private Connection connection ;
     private ResultSet rs;
     private PreparedStatement ps;
@@ -12,8 +14,8 @@ public class GestionVoiture {
 
     public void BeginConnection(String url, String user, String password) throws SQLException, ClassNotFoundException {
 //            sert a créer la connexion entre la base de donnée et le programme
-       Class.forName("com.mysql.cj.jdbc.Driver");
-       connection = DriverManager.getConnection(url, user, password);
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection = DriverManager.getConnection(url, user, password);
     }
 
     public ArrayList<Voiture> FillList(ArrayList<Voiture> v) throws SQLException {
@@ -25,9 +27,112 @@ public class GestionVoiture {
             Voiture voiture = new Voiture(rs.getInt("id"), rs.getString("model"), m, rs.getInt("nb_vente"));
             v.add(voiture);
         }
-        rs.close();
-        ps.close();
-        connection.close();
+        return v;
+    }
+    public ArrayList<Voiture> SortBySells() throws SQLException {
+        ArrayList<Voiture> result = new ArrayList<>();
+        ps = connection.prepareStatement("SELECT nb_vente, model, marque.nom FROM voiture JOIN marque ON voiture.id_marque = marque.id Order BY nb_vente DESC");
+        rs = ps.executeQuery();
+        while(rs.next()){
+            Voiture v = new Voiture();
+            v.setNb_vente(rs.getInt("nb_vente"));
+            v.setModele(rs.getString("model"));
+            v.setMarque(new Marque(0, rs.getString("marque.nom")));
+            result.add(v);
+        }
+        return result;
+    }
+    public ArrayList<Voiture> PrintCars() throws SQLException {
+        ArrayList<Voiture> v =new ArrayList<>();
+        String demande ;
+        System.out.println("""
+        quel est la marque de voiture dont vous voulez savoir les modèles ? 
+        vous avez le choix parmis :
+        Renault
+        Citroën
+        Toyota 
+        Peugeot
+        Dacia
+        """);
+        demande = In.readString();
+        ps = connection.prepareStatement("SELECT marque.nom, model FROM voiture JOIN marque ON voiture.id_marque = marque.id WHERE marque.nom = " + demande);
+        rs = ps.executeQuery();
+        while(rs.next()){
+            Voiture vo = new Voiture();
+            vo.setModele(rs.getString("model"));
+            vo.setMarque(new Marque(0, rs.getString("marque.nom")));
+            v.add(vo);
+        }
+        return v;
+    }
+    public ArrayList<Voiture> PrintSumCarSell() throws SQLException {
+        ArrayList<Voiture> v = new ArrayList<>();
+        String demande ;
+        System.out.println("""
+        quel est la marque de voiture dont vous voulez savoir les modèles ? 
+        vous avez le choix parmis :
+        Renault
+        Citroën
+        Toyota 
+        Peugeot
+        Dacia
+        """);
+        demande = In.readString();
+        ps = connection.prepareStatement("SELECT SUM(nb_vente) FROM voiture JOIN marque ON voiture.id_marque = marque.id WHERE marque.nom = "+ demande);
+        rs = ps.executeQuery();
+        while(rs.next()){
+
+        }
+        return v;
+    }
+    public ArrayList<Voiture> PrintCarByBeginningLetter(ArrayList<Voiture> v) throws SQLException {
+        char demande ;
+        System.out.println("quel est la lettre que vous voulez choisir dans votre recherche de modele ");
+        demande = In.readChar();
+        ps = connection.prepareStatement("SELECT marque.nom, model FROM voiture JOIN marque ON voiture.id_marque = marque.id WHERE model LIKE '"+ demande + "'");
+        rs = ps.executeQuery();
+        return v;
+    }
+    public int PrintNumberOfSaves() throws SQLException {
+        int result = 0;
+        ps = connection.prepareStatement("SELECT COUNT(*) AS nombre FROM voiture JOIN marque ON voiture.id_marque = marque.id");
+        rs = ps.executeQuery();
+        while(rs.next()){
+            result = rs.getInt("nombre");
+        }
+        return result;
+    }
+    public int PrintSumAllSells() throws SQLException {
+        int result  = 0;
+        ps = connection.prepareStatement("SELECT SUM(nb_vente) AS nombre FROM voiture JOIN marque ON voiture.id_marque = marque.id");
+        rs = ps.executeQuery();
+        while(rs.next()){
+            result = rs.getInt("nombre");
+        }
+        return result;
+    }
+    public ArrayList<Voiture> PrintCarByLetters(ArrayList<Voiture> v) throws SQLException {
+        char demande ;
+        System.out.println("quel est la lettre que vous choisicez pour votre recherche de modele ");
+        demande = In.readChar();
+        ps = connection.prepareStatement("SELECT marque.nom, model FROM voiture JOIN marque ON voiture.id_marque = marque.id WHERE model like '%"+ demande+"%'");
+        rs = ps.executeQuery();
+        return v;
+    }
+    public ArrayList<Voiture> PrintCarBySecondLetters(ArrayList<Voiture> v) throws SQLException {
+        char demande ;
+        System.out.println("quel est la lettre que vous choisirez dans votre recherche de modele ");
+        demande = In.readChar();
+        ps = connection.prepareStatement("SELECT marque.nom, model FROM voiture JOIN marque ON voiture.id_marque = marque.id WHERE model LIKE '_"+ demande+"%'");
+        rs = ps.executeQuery();
+        return v;
+    }
+    public ArrayList<Voiture> PrintCarByLastLetter(ArrayList<Voiture> v) throws SQLException {
+        char demande ;
+        System.out.println("quel est la lettre que vous choisirez pour votre recherche de modele ");
+        demande = In.readChar();
+        ps = connection.prepareStatement("SELECT marque.nom, model FROM voiture JOIN marque ON voiture.id_marque = marque.id WHERE model LIKE '%"+demande+"'");
+        rs = ps.executeQuery();
         return v;
     }
 }
--- src/main/java/fr/btsciel/Ihm.java
+++ src/main/java/fr/btsciel/Ihm.java
@@ -15,15 +15,15 @@ public class Ihm {
         ArrayList<Voiture> v = new ArrayList<>();
 
 //            sert a créer la connexion entre la base de donnée et le programme et a le remplir
-        System.out.println("quel est l'url de connexion de la base de donnée ? ");
-        url = In.readString();
-        System.out.println("quel est le nom du user ? ");
-        user = In.readString();
-        System.out.println(" quel est le password du user ? ");
-        password = In.readString();
+//        System.out.println("quel est l'url de connexion de la base de donnée ? ");
+//        url = In.readString();
+//        System.out.println("quel est le nom du user ? ");
+//        user = In.readString();
+//        System.out.println(" quel est le password du user ? ");
+//        password = In.readString();
 
         try {
-            gv.BeginConnection(url, user, password);
+            gv.BeginConnection("jdbc:mysql://localhost/ventes_de_voitures_2", "root", "");
             gv.FillList(v);
         } catch (SQLException | ClassNotFoundException e) {
             throw new RuntimeException(e);
@@ -42,27 +42,68 @@ public class Ihm {
                 7| afficher les voiture dont le modele contient une lettre donnée
                 8| afficher les modele de voiture qui ont pour deuxième lettre, une lettre donnée
                 9| afficher tout les modeles ce terminant par une lettre donnée
+                10| ajouter une nouvelle voiture
                 """);
         menu = In.readInteger();
-        switch(menu) {
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            case 7:
-                break;
-            case 8:
-                break;
-            case 9:
-                break;
+        try{
+            switch(menu) {
+                case 1:
+                    ArrayList<Voiture> sells = gv.SortBySells();
+                    for (Voiture voiture : sells) {
+                        System.out.print(voiture.getNb_vente() + "\t");
+                        System.out.print(voiture.getModele() + "\t");
+                        System.out.println(voiture.getMarque().getNom() );
+                    }
+                    break;
+                case 2:
+                    ArrayList<Voiture> cars = gv.PrintCars();
+                    for (Voiture voiture : cars) {
+                        System.out.print(voiture.getModele() + "\t");
+                        System.out.println(voiture.getMarque().getNom());
+                    }
+                    break;
+                case 3:
+                    gv.PrintSumCarSell();
+                    for(Voiture voiture : v){
+                        System.out.println(voiture.getNb_vente());
+                    }
+                    break;
+                case 4:
+                    gv.PrintCarByBeginningLetter(v);
+                    for(Voiture voiture : v){
+                        System.out.println(voiture);
+                    }
+                    break;
+                case 5:
+                    System.out.println(gv.PrintNumberOfSaves());
+                    break;
+                case 6:
+                    System.out.println(gv.PrintSumAllSells());
+                    break;
+                case 7:
+                    gv.PrintCarByLetters(v);
+                    for(Voiture voiture : v){
+                        System.out.println(voiture.getModele());
+                    }
+                    break;
+                case 8:
+                    gv.PrintCarBySecondLetters(v);
+                    for(Voiture voiture : v){
+                        System.out.println(voiture.getModele());
+                    }
+                    break;
+                case 9:
+                    gv.PrintCarByLastLetter(v);
+                    for(Voiture voiture : v){
+                        System.out.println(voiture.getModele());
+                    }
+                    break;
+                case 10:
+
+                    break;
+            }
+        } catch (SQLException e) {
+            System.err.println("votre liste est vide " + e.getMessage());
         }
     }
 }
